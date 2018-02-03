413d02739e87be6f2419e0610252d458d5b6ca05
Jiafeng Zhong <jz16199@my.bristol.ac.uk>
2018-02-03 11:15:34 +0800

    new

--- testVSCode/1.js
+++ testVSCode/1.js
@@ -1,13 +1,14 @@
 function m(a, b) {
     return x + y;
 }
-
+let a = 1
 /**
  * 
  * 
  * @export
  * @param {any} a 
  */
-export function() {
-    name = 'a' 
+export function(a) {
+    name = a
+    return name
 }
